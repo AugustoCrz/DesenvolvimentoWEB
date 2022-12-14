ec0abcaa4399cfc32a5d09f82b8da0d8229ae9f1
AugustoCrz <56053617+AugustoCrz@users.noreply.github.com>
2022-12-14 20:07:52 -0300

    Itens fantasmas

--- Desktop/dsw/app-api/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app.js");
-require("dotenv").config();
-
-const port = process.env.SERVER_PORT || 8085;
-
-app.listen(port, () => {
-    console.log(`Server ativo na porta ${port}`);
-});
